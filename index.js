7ecb697cdfa16551dd39a67bc34cf008b03dff79
tracykaur <tracykaur@yahoo.com>
2017-12-31 19:38:28 +1000

    added extra route handler

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const GoogleStrategy = require('passport-google-oauth20').S trategy;
 const keys = require('./config/keys');
 const app = express();
 
@@ -23,5 +23,8 @@ app.get(
   })
 );
 
+app.get(
+  '/auth/google/callback', passport.authenticate('google'));
+   
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
