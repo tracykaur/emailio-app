0f4053aff5751e1df8d02f6dcf64243bfd0067be
tracykaur <tracykaur@yahoo.com>
2018-01-06 23:45:10 +1000

    added User Class

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
+require('./models/User');
 require('./services/passport');
 
-mongoose.connect(keys.mongoURI);
+mongoose.connect(keys.mongoUri);
 const app = express();
 
 require('./routes/authRoutes')(app);
--- models/User.js
+++ models/User.js
@@ -2,5 +2,7 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  googleId: String
+  googleID: String
 });
+
+mongoose.model('users', userSchema);
